2ce5d75f8f19b73b319c3c46b65d56c5703fe03a
Kacper <kziejlo@gmail.com>
2021-07-04 12:47:33 +0200

    Updated Security a bit

--- src/main/java/pwr/PracaInz/Security/SecurityConfiguration.java
+++ src/main/java/pwr/PracaInz/Security/SecurityConfiguration.java
@@ -8,25 +8,19 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -62,8 +56,8 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
                 .headers().frameOptions().sameOrigin()
                 .and()
                 .authorizeRequests()
-                .mvcMatchers( "/login", "/anime/**", "/anime**", "/logoutUser").permitAll()
-                .mvcMatchers("/forum/**", "/forum**", "/aa", "/aa**").authenticated()
+                .mvcMatchers( "/login", "/logoutUser", "/anime/**", "/anime**").permitAll()
+                .mvcMatchers("/forum/**", "/forum**").authenticated()
                 .anyRequest().authenticated()
         ;
 
@@ -71,19 +65,15 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        List<String> arr = new ArrayList<>();
-        arr.add("http://localhost:3000");
-        arr.add("http://localhost:8180");
-
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(arr);
-        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "OPTIONS", "DELETE", "PUT", "HEAD", "PATCH"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8180"));
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "OPTIONS", "DELETE", "PUT"));
         configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Origin", "X-Requested-With", "Content-Type", "Accept", "If-Match", "remember-me", "Authorization", "ETag", "Referer"));
-        configuration.addExposedHeader("*");
+        configuration.setAllowedHeaders(Arrays.asList(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION));
+        configuration.addExposedHeader(HttpHeaders.AUTHORIZATION);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-        source.registerCorsConfiguration("/*", configuration);
         return source;
     }
 }
