d6cc6faea84d6da0c7b417317f956e4e198ffada
Kacper <kziejlo@gmail.com>
2021-11-07 15:14:36 +0100

    Refactor: small corrections to run configurations

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# shellcheck disable=SC2164
-cd -
-cd -
+#Must be run in Server folder
 
+#build again with static content
+SERVE_STATIC_CONTENT=serve gradle clean build
+
+#Use docker compose to set all up, with the anime app using the fresh files
 docker-compose -f docker/docker-compose-all.yml up --build
